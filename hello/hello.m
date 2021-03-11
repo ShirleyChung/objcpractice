17cbe4a0623b864a79c9b8891655375b3aa58ae8
ShirleyChung <shirley@mail.kway.com>
2021-03-11 10:43:42 +0800

    [hello] use property
    [why] property and its attribute
    [how] nonatomic, readwrite, copy/strong/weak/assign/unsafe_unretained
    [ems]
    [note]
    [setting]

--- hello/hello.m
+++ hello/hello.m
@@ -1,7 +1,20 @@
-#import <Foundation/Foundation.h>
+#import "hello.h"
+
+
+@implementation Person
+
+@end
 
 int main(int argc, char* argv[])
 {
-	NSLog(@"Hello!");
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+	Person* p = [[Person alloc] init];
+	
+	p.name = @"OBJC!";
+
+	NSLog(@"Hello! %@", p.name);
+
+	[pool drain];
 	return 0;
 }
